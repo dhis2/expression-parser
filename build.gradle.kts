1797d02dd6fa9af6ece0006426e186f2316954fc
Victor Garcia <vgarciabnz@gmail.com>
2023-09-07 12:25:15 +0200

    Move to KMP structure

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+group = "org.hisp.dhis.lib.expression"
+version = "1.1.0-SNAPSHOT"
+
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    id("io.freefair.lombok") version "8.3"
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+        testRuns.named("test") {
+            executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
+    }
+    js {
+        nodejs()
+        useEsModules()
+        binaries.library()
+    }
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+    }
+}
