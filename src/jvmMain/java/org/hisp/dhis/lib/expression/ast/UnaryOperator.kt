f17eefdda2c8965aa343135c78399e7388dd8ae1
Jan Bernitt <jaanbernitt@gmail.com>
2023-09-25 13:48:04 +0200

    UnaryOperator

---
+++ src/jvmMain/java/org/hisp/dhis/lib/expression/ast/UnaryOperator.kt
@@ -0,0 +1,35 @@
+package org.hisp.dhis.lib.expression.ast
+
+import org.hisp.dhis.lib.expression.spi.ValueType
+
+enum class UnaryOperator(@JvmField val symbol: String, private val returnType: ValueType) : Typed {
+
+    PLUS("+", ValueType.NUMBER),
+    MINUS("-", ValueType.NUMBER),
+    NOT("!", ValueType.BOOLEAN),
+    DISTINCT("distinct", ValueType.SAME);
+
+    override fun getValueType(): ValueType {
+        return returnType
+    }
+
+    companion object {
+        /**
+         * Avoid defensive copy when finding operator by symbol
+         */
+        private val VALUES = listOf(*entries.toTypedArray())
+        @JvmStatic
+        fun fromSymbol(symbol: String): UnaryOperator {
+            return if ("not" == symbol) NOT else VALUES.stream().filter { op: UnaryOperator -> op.symbol == symbol }
+                .findFirst().orElseThrow()
+        }
+
+        @JvmStatic
+        fun negate(value: Number?): Number? {
+            if (value == null) {
+                return null
+            }
+            return if (value is Int) Math.negateExact(value.toInt()) else -value.toDouble()
+        }
+    }
+}
