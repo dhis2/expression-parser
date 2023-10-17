85f6fb36065b9b6ee7508f6506418f6409ce5ec9
Jan Bernitt <jaanbernitt@gmail.com>
2023-10-17 18:56:43 +0200

    test: d2:maxValue function tests

---
+++ src/commonTest/kotlin/org/hisp/dhis/lib/expression/function/MaxValueTest.kt
@@ -0,0 +1,26 @@
+package org.hisp.dhis.lib.expression.function
+
+import org.hisp.dhis.lib.expression.spi.ValueType
+import org.hisp.dhis.lib.expression.util.RuleVariableValue
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * Test of the `d2:maxValue` function.
+ *
+ * @author Jan Bernitt
+ */
+class MaxValueTest : AbstractVariableBasedTest() {
+
+    @Test
+    fun testMaxValue_Empty() {
+        val values = mapOf("v1" to RuleVariableValue(ValueType.NUMBER).copy(candidates = listOf()))
+        assertEquals(Double.NaN, evaluate("d2:maxValue(#{v1})", values))
+    }
+
+    @Test
+    fun testMaxValue() {
+        val values = mapOf("v1" to RuleVariableValue(ValueType.NUMBER).copy(candidates = listOf("1", "42", "-100")))
+        assertEquals(42.0, evaluate("d2:maxValue(#{v1})", values))
+    }
+}
