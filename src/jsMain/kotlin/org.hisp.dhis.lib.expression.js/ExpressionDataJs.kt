e5db4998870c3e09d9214d0ba076c337c7a5dac8
Jan Bernitt <jaanbernitt@gmail.com>
2023-10-25 16:48:07 +0200

    fix: build

--- src/jsMain/kotlin/org.hisp.dhis.lib.expression.js/ExpressionDataJs.kt
+++ src/jsMain/kotlin/org.hisp.dhis.lib.expression.js/ExpressionDataJs.kt
@@ -3,12 +3,9 @@ package org.hisp.dhis.lib.expression.js
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 data class ExpressionDataJs(
-    val programRuleVariableValues: Array<Entry<String, VariableValueJs>>,
-    val programVariableValues: Array<Entry<String, Any>>,
-    val supplementaryValues: Array<Entry<String, Array<String>>>,
-    val dataItemValues: Array<Entry<DataItemJs, Any>>,
-    val namedValues: Array<Entry<String, Any>>
-) {
-    @JsName("EMPTY")
-    constructor() : this(emptyArray(), emptyArray(), emptyArray(), emptyArray(), emptyArray())
-}
+    val programRuleVariableValues: Array<Entry<String, VariableValueJs>> = emptyArray(),
+    val programVariableValues: Array<Entry<String, Any>> = emptyArray(),
+    val supplementaryValues: Array<Entry<String, Array<String>>> = emptyArray(),
+    val dataItemValues: Array<Entry<DataItemJs, Any>> = emptyArray(),
+    val namedValues: Array<Entry<String, Any>> = emptyArray()
+)
