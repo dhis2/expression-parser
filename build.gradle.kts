a60e94e2116365ddad08d121eb368abfa09b3d2d
Victor Garcia <vgarciabnz@gmail.com>
2023-10-26 13:57:17 +0200

    fix: update sonatype url

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,6 +88,8 @@ publicationConfig(dokkaHtmlJar)
 nexusPublishing {
     this.repositories {
         sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(ossrhUsername)
             password.set(ossrhPassword)
         }
