f1ac4a3808e4f0f00787cc61bec79610c9a1d5d4
Jan Bernitt <jaanbernitt@gmail.com>
2023-11-29 14:17:16 +0100

    test: moved from rule engine repo

---
+++ src/commonTest/kotlin/org/hisp/dhis/lib/expression/math/GS1ElementsTest.kt
@@ -0,0 +1,29 @@
+package org.hisp.dhis.lib.expression.math
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class GS1ElementsTest {
+
+    @Test
+    fun shouldReturnGS1Element() {
+        assertEquals(GS1Elements.GTIN, GS1Elements.fromKey("gtin"))
+        assertEquals(GS1Elements.GTIN, GS1Elements.fromKey("GTIN"))
+        assertEquals(GS1Elements.LOT_NUMBER, GS1Elements.fromKey("lot number"))
+        assertEquals(GS1Elements.LOT_NUMBER, GS1Elements.fromKey("LOT_NUMBER"))
+        assertEquals(GS1Elements.LOT_NUMBER, GS1Elements.fromKey("LOT NUMBER"))
+        assertEquals(GS1Elements.AREA_I2, GS1Elements.fromKey("AREA I2"))
+        assertEquals(GS1Elements.PAY_TO, GS1Elements.fromKey("PAY_TO"))
+        assertEquals(GS1Elements.PAY_TO, GS1Elements.fromKey("PAY TO"))
+        assertEquals(GS1Elements.PAY_TO, GS1Elements.fromKey("pay to"))
+        assertEquals(GS1Elements.AREA_I2, GS1Elements.fromKey("AREA I2"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("product url"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("product_url"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("productUrl"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("ProductUrl"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("Product_Url"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("PRODUCT_URL"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("PRODUCT URL"))
+        assertEquals(GS1Elements.PRODUCT_URL, GS1Elements.fromKey("PRODUCTURL"))
+    }
+}
