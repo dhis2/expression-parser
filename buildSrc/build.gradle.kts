c81f99969554145d6d27567a40a1a73685d3a33c
Victor Garcia <victor.garcia@dhis2.org>
2025-01-22 10:15:33 +0100

    Create version catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
-    implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
-    implementation("dev.petuska:npm-publish-gradle-plugin:3.4.1")
+    implementation(libs.kotlin.plugin)
+    implementation(libs.dokka.plugin)
+    implementation(libs.nexusPublish.plugin)
+    implementation(libs.npmPublish.plugin)
 }
